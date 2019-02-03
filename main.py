983634b164d30b9b7bcaf2ac9f1fbbdc14398380
sujunglee <sujunglee2020@u.northwestern.edu>
2019-02-03 17:27:35 -0600

    Read and tally up attendance from single csv file

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 import csv
+
+# The dictionary that will hold all of the member points
+member_points = {}  # type: dict
+
+#############################################
+## PART 1: ATTENDANCE TO REGULAR WIC EVENT ##
+#############################################
+
+input_file = csv.DictReader(open("../checkin-forms/intro-to-tech.csv"))
+
+for row in input_file:
+    email = row["Email Address"]
+    if email in member_points:
+        member_points[email] += 1
+    else:
+        member_points[email] = 1
+    print("\n")
+
+for key, value in member_points.items():
+    print("{}: {}".format(key, value))
+    print("\n")
